e3dd6d2e929a7118d96ebea8d429a3a78fb07d3a
Ancoder2009 <68029104+Ancoder2009@users.noreply.github.com>
2021-05-13 11:02:31 -0700

    Update check.auth.js

--- account/check.auth.js
+++ account/check.auth.js
@@ -1,1 +1,28 @@
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return null;
+}
 
+if (getCookie == null) {} else {
+	xhttp.open("POST", "https://auth.scratchblox.tk/auth", true);
+	xhttp.setRequestHeader("Content-type", "");
+	xhttp.send();
+	xhttp.onreadystatechange = function() {
+  		if (this.readyState == 4 && this.status == 200) {
+    		console.log(this.responseText);
+  		} else {
+			console.log("Invalid")
+		}
+	};
+}
