29e91ce5ac1ce64df27fd99158d83832d97bea86
Ancoder2009 <68029104+Ancoder2009@users.noreply.github.com>
2021-05-13 11:13:50 -0700

    Update check.auth.js

--- account/check.auth.js
+++ account/check.auth.js
@@ -14,7 +14,7 @@ function getCookie(cname) {
   return null;
 }
 
-if (getCookie == null) {} else {
+if (getCookie(scratchstoken) == null) {} else {
 	var xhttp = new XMLHttpRequest
 	xhttp.open("POST", "https://auth.scratchblox.tk/auth", true);
 	xhttp.setRequestHeader("Content-type", "");
