15602ee567fe8572aeb0178ae50011becdc88fad
Ancoder2009 <68029104+Ancoder2009@users.noreply.github.com>
2021-05-13 12:01:43 -0700

    Create write.auth.js

---
+++ account/write.auth.js
@@ -0,0 +1,34 @@
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return null;
+}
+
+if (getCookie(scratchstoken) == null) {} else {
+	var xhttp = new XMLHttpRequest
+	xhttp.open("POST", "https://auth.scratchblox.tk/auth", true);
+	xhttp.setRequestHeader("Content-type", "");
+	xhttp.send();
+	xhttp.onreadystatechange = function() {
+  		if (this.readyState == 4 && this.status == 200) {
+    			window.location.assign("https://web.scratchblox.tk/home")
+  		} else {}
+	};
+}
+
+document.body.onload = () => {
+const message = document.getElementById("message")
+if (getCookie("regmsg") == null) {} else {
+	message.innerHTML = getCookie("regmsg")
+}
+}
